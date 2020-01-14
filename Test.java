1de88bfa9fbcfc077a7b3cb7218c2008254a30f1
776903455 <776903455@qq.com>
2020-01-14 17:42:02 +0800

    修改

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        new Thread(() -> System.out.println(Thread.currentThread().getName())).start();
+        System.out.println("我来看看该了没");
 
 
 
